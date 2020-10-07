a79422b015879cf8a59842be3bd2f4665a5286aa
Amey Sunu <ameysunu00719@gmail.com>
2020-10-08 01:09:57 +0300

    test fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,13 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:youtube_flutter/main.dart';
 
 void main() {
-  // testWidgets('smoke test', (WidgetTester tester) async {
-  //   final app = MyApp();
-  //   await tester.pumpWidget(app);
-  //   expect(find.text("0"), findsOneWidget);
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //   expect(find.text("1"), findsOneWidget);
-  // });
-  runApp(MyApp());
+  testWidgets('smoke test', (WidgetTester tester) async {
+    runApp(MyApp());
+  });
 }
