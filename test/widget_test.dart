ed48d521d853c797c92180a4d2db18ecdfcba0c1
Amey Sunu <ameysunu00719@gmail.com>
2020-10-08 00:58:55 +0300

    travis ci added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,20 +10,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:youtube_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
+  testWidgets('smoke test', (WidgetTester tester) async {
+    final app = MyApp();
+    await tester.pumpWidget(app);
+    expect(find.text("0"), findsOneWidget);
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("1"), findsOneWidget);
   });
 }
